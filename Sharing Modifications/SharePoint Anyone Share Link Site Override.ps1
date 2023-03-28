40d08bebca358d4eca3d852a14503d38abec7940
Tyler Duncan <tyler.duncan@outlook.com>
2023-03-28 12:43:23 -0500

    SharePoint Script to Change Anywhere Link Sharing Defaults

---
+++ Sharing Modifications/SharePoint Anyone Share Link Site Override.ps1
@@ -0,0 +1,37 @@
+﻿#requires -modules PnP.PowerShell
+#requires -PSEdition Core
+
+# This Sample Code is provided for the purpose of illustration only and is not intended to be used
+# in a production environment. THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS"
+# WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE. We grant You a nonexclusive,
+# royalty-free right to use and modify the Sample Code and to reproduce and distribute the object code
+# form of the Sample Code, provided that You agree: (i) to not use Our name, logo, or trademarks to
+# market Your software product in which the Sample Code is embedded; (ii) to include a valid copyright
+# notice on Your software product in which the Sample Code is embedded; and (iii) to indemnify, hold
+# harmless, and defend Us and Our suppliers from and against any claims or lawsuits, including attorneys'
+# fees, that arise or result from the use or distribution of the Sample Code.
+
+# This sample script is not supported under any Microsoft standard support program or service.
+# The sample script is provided AS IS without warranty of any kind. Microsoft further disclaims
+# all implied warranties including, without limitation, any implied warranties of merchantability
+# or of fitness for a particular purpose. The entire risk arising out of the use or performance of
+# the sample scripts and documentation remains with you. In no event shall Microsoft, its authors,
+# or anyone else involved in the creation, production, or delivery of the scripts be liable for any
+# damages whatsoever (including, without limitation, damages for loss of business profits, business
+# interruption, loss of business information, or other pecuniary loss) arising out of the use of or
+# inability to use the sample scripts or documentation, even if Microsoft has been advised of the
+# possibility of such damages.
+
+
+param 
+( 
+    $AdminUrl = 'https://m365edu323659-admin.sharepoint.com' # m365edutechdemo.org   
+)
+
+# Connect to SharePoint Admin
+Connect-PnPOnline -Url $AdminUrl -Interactive
+
+$script = '{"$schema": "schema.json", "actions": [{"verb": "setSiteExternalSharingCapability", "capability": "ExternalUserAndGuestSharing"}], "bindata":{}, "version": 1};'
+Add-PnPSiteScript -Title 'ExternalUserAndGuestSharing site script' -Description 'Site with ExternalUserAndGuestSharing' -Content $script -OutVariable sitescript
+Add-PnPSiteDesign -Title 'Modern Team site with External User and Guest Sharing Enabled' -WebTemplate '64' -SiteScriptIds $sitescript.id.guid -IsDefault
